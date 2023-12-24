f2c234e9bcd51fb0db6eaedb3f62ac8a4dae77f3
Zolo <39656359+zolodev@users.noreply.github.com>
2023-12-24 23:35:40 +0100

    Bugfix file path

--- install-codium-extensions.ps1
+++ install-codium-extensions.ps1
@@ -1,2 +1,2 @@
 Get-Content extensions.txt | ForEach-Object { codium --install-extension $_ }
-Get-ChildItem -r ./vsxi | ForEach-Object { codium --install-extension $_ }
+Get-ChildItem -r ./vsxi | ForEach-Object { codium --install-extension $_.FullName }
